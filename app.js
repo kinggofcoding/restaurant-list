15c5b32d972a96284d32c3c7ac7e8c9664b6bc6d
jay <intuitionpz@gmail.com>
2023-11-25 16:19:14 +0800

    project init

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+
