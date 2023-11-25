e4db0dc562ca5d4be5d98103899a468489c225ec
jay <intuitionpz@gmail.com>
2023-11-25 16:35:06 +0800

    build index page

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
+const cssIndex = '/stylesheets/index.css'
+const cssShow = '/stylesheets/show.css'
+
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
@@ -9,3 +13,11 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 
+app.get('/', (req, res) => {
+    res.render('index', { restaurants: restaurants, cssPath: cssIndex })
+})
+
+
+app.listen(port, () => {
+    console.log(`express server is running on http://localhost:${port}`)
+  })
